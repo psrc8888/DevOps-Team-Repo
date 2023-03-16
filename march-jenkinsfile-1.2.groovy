02195f56c328a2bf5907b7d90188ee753b9ae14a
Subbu <psrc8888@gmail.com>
2023-03-16 01:59:53 -0400

    declarative jenkins pipeline

---
+++ march-jenkinsfile-1.2.groovy
@@ -0,0 +1,25 @@
+pipeline {
+    agent any
+    parameters {
+        choice(name: 'VERSION', choices: ['1.0', '1.1', '1.2'], description: 'declarative pipeline')
+        booleanParam(name: 'Run', defaultValue: true, description: 'boolean')
+    }
+    stages {
+        stage('Clone Application Code') {
+            steps {
+                git 'https://github.com/psrc8888/Development-Team-Repo.git'
+            }
+        }
+        stage('Build') {
+            steps {
+                sh 'mvn package -f pom.xml'
+            }
+        }
+        stage('Deploy') {                 // copy web.war file to tomcat server
+            steps {
+                echo 'Deploy Application Code'
+                sh 'sudo cp /var/lib/jenkins/workspace/secondpipeline/target/web.war /root/apache-tomcat-10.1.7/webapps'
+            }
+        }
+    }
+}
